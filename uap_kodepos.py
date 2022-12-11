c4386c8cb0a3bb46ad6266b6cb573c6772eb5f49
Muhammad Iqbal Widiansyah <92410990+MuhammadIqbalWidiansyah@users.noreply.github.com>
2022-12-12 00:56:43 +0700

    Update uap_kodepos.py

--- uap_kodepos.py
+++ uap_kodepos.py
@@ -30,3 +30,33 @@ namaProv1 = input("\nMasukkan Nama Provinsi\t: ")
 prov1 = Provinsi(namaProv1.title())
 prov1.getKeyProv()
 keyProv = prov1.getKeyProv()
+
+# Kota/Kabupaten
+ 
+linkKota = "https://kodepos-2d475.firebaseio.com/list_kotakab/"+keyProv+".json"
+hasilKota = requests.get(linkKota)
+kota = hasilKota.json()
+
+print('\nDaftar Kota/Kabupaten\n')
+
+for i in kota:
+    print(kota[i])
+ 
+class Kota:
+    def __init__(self, namaKota):
+        self.namaKota = namaKota
+        self.linkKota = linkKota
+        self.hasilKota = hasilKota
+        self.kota = kota
+ 
+    def getKeyKota(self):
+        for key, value in self.kota.items():
+            if self.namaKota == value:
+                return key
+ 
+        return "Key not found"
+ 
+namaKota1 = input("\nMasukkan Nama Kota\t: ")
+kota1 = Kota(namaKota1.title())
+kota1.getKeyKota()
+keyKota = kota1.getKeyKota()
