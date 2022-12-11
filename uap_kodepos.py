4cd6b5b6e18d324f31b262e31630d2144e656d6f
Faiz Muzaki <34159640+faizmuzaki@users.noreply.github.com>
2022-12-12 00:57:11 +0700

    add kodepos

--- uap_kodepos.py
+++ uap_kodepos.py
@@ -60,3 +60,34 @@ namaKota1 = input("\nMasukkan Nama Kota\t: ")
 kota1 = Kota(namaKota1.title())
 kota1.getKeyKota()
 keyKota = kota1.getKeyKota()
+
+# Kecamatan/Kelurahan
+ 
+linkPos = "https://kodepos-2d475.firebaseio.com/kota_kab/"+keyKota+".json"
+hasilPos = requests.get(linkPos)
+pos = hasilPos.json()
+
+print('\nDaftar Kecamatan/Kelurahan\n')
+
+for i in pos:
+    print('Kecamatan\t:', i['kecamatan'], '\nKelurahan\t:', i['kelurahan'], '\n')
+
+class Pos:
+    def __init__(self, namaLurah):
+        self.namaLurah = namaLurah
+        self.linkPos = linkPos
+        self.hasilPos = hasilPos
+        self.pos = pos
+ 
+    def getKeyPos(self):
+        for i in pos:
+            if self.namaLurah == i['kelurahan']:
+                print('\nKecamatan\t:', i['kecamatan'], '\nKelurahan\t:', i['kelurahan'], 
+                '\nKode Pos\t:', i['kodepos'], '\n')
+                f = open("history.txt", "a")
+                f.write("\nKecamatan\t:"+ i['kecamatan']+ "\nKelurahan\t:"+ i['kelurahan']+"\nKode Pos\t:"+ i['kodepos']+ "\n")
+                f.close()
+ 
+namaLurah1 = input("Masukkan Nama Kelurahan\t: ")
+pos1 = Pos(namaLurah1.title())
+pos1.getKeyPos()
