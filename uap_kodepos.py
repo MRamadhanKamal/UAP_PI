83d3da83ba75aa91c6943dd347fa6d3d440951cd
Muhamad Ramadhan Kamal <91411703+MRamadhanKamal@users.noreply.github.com>
2022-12-13 19:40:13 +0700

    membuat menu

--- uap_kodepos.py
+++ uap_kodepos.py
@@ -1,16 +1,6 @@
 import requests
 import json
- 
-# Provinsi
- 
-linkProv = "https://kodepos-2d475.firebaseio.com/list_propinsi.json"
-getProv = requests.get(linkProv)
-prov = getProv.json()
-
-print('Daftar Provinsi\n')
-
-for i in prov:
-    print(prov[i])
+import os
 
 class Provinsi:
     def __init__(self, namaProv):
@@ -25,22 +15,7 @@ class Provinsi:
                 return key
  
         return "Key not found"
- 
-namaProv1 = input("\nMasukkan Nama Provinsi\t: ")
-prov1 = Provinsi(namaProv1.title())
-prov1.getKeyProv()
-keyProv = prov1.getKeyProv()
 
-# Kota/Kabupaten
- 
-linkKota = "https://kodepos-2d475.firebaseio.com/list_kotakab/"+keyProv+".json"
-hasilKota = requests.get(linkKota)
-kota = hasilKota.json()
-
-print('\nDaftar Kota/Kabupaten\n')
-
-for i in kota:
-    print(kota[i])
  
 class Kota:
     def __init__(self, namaKota):
@@ -56,21 +31,7 @@ class Kota:
  
         return "Key not found"
  
-namaKota1 = input("\nMasukkan Nama Kota\t: ")
-kota1 = Kota(namaKota1.title())
-kota1.getKeyKota()
-keyKota = kota1.getKeyKota()
 
-# Kecamatan/Kelurahan
- 
-linkPos = "https://kodepos-2d475.firebaseio.com/kota_kab/"+keyKota+".json"
-hasilPos = requests.get(linkPos)
-pos = hasilPos.json()
-
-print('\nDaftar Kecamatan/Kelurahan\n')
-
-for i in pos:
-    print('Kecamatan\t:', i['kecamatan'], '\nKelurahan\t:', i['kelurahan'], '\n')
 
 class Pos:
     def __init__(self, namaLurah):
@@ -87,7 +48,20 @@ class Pos:
                 f = open("history.txt", "a")
                 f.write("\nKecamatan\t:"+ i['kecamatan']+ "\nKelurahan\t:"+ i['kelurahan']+"\nKode Pos\t:"+ i['kodepos']+ "\n")
                 f.close()
- 
-namaLurah1 = input("Masukkan Nama Kelurahan\t: ")
-pos1 = Pos(namaLurah1.title())
-pos1.getKeyPos()
+
+
+while(True):
+    os.system('cls')
+    listMenu = ["1. Cari Kode Pos", "2. Credits", "3. Keluar"]
+    print("\t ~~~~KATALOG KODE POS~~~~\n\n")
+    print("Menu Katalog Kode Pos")
+    print("\t",listMenu[0])
+    print("\t",listMenu[1])
+    print("\t",listMenu[2])
+    while(True):
+        try:
+            pilih = int(input("masukkan pilihan: "))
+            break
+        except (TypeError, ValueError):
+            print("Invalid Input")
+            x = input()
