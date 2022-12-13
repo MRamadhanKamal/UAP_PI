346f4ac57e0f1564a66c6e1581dfec8de2944d2e
Muhamad Ramadhan Kamal <91411703+MRamadhanKamal@users.noreply.github.com>
2022-12-13 21:16:04 +0700

    Update uap_kodepos.py

--- uap_kodepos.py
+++ uap_kodepos.py
@@ -16,39 +16,6 @@ class Provinsi:
  
         return "Key not found"
 
- 
-class Kota:
-    def __init__(self, namaKota):
-        self.namaKota = namaKota
-        self.linkKota = linkKota
-        self.hasilKota = hasilKota
-        self.kota = kota
- 
-    def getKeyKota(self):
-        for key, value in self.kota.items():
-            if self.namaKota == value:
-                return key
- 
-        return "Key not found"
- 
-
-
-class Pos:
-    def __init__(self, namaLurah):
-        self.namaLurah = namaLurah
-        self.linkPos = linkPos
-        self.hasilPos = hasilPos
-        self.pos = pos
- 
-    def getKeyPos(self):
-        for i in pos:
-            if self.namaLurah == i['kelurahan']:
-                print('\nKecamatan\t:', i['kecamatan'], '\nKelurahan\t:', i['kelurahan'], 
-                '\nKode Pos\t:', i['kodepos'], '\n')
-                f = open("history.txt", "a")
-                f.write("\nKecamatan\t:"+ i['kecamatan']+ "\nKelurahan\t:"+ i['kelurahan']+"\nKode Pos\t:"+ i['kodepos']+ "\n")
-                f.close()
-
 
 while(True):
     os.system('cls')
