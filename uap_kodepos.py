c32b98b708b428538694311ffa140c61c39e1be3
Faiz Muzaki <34159640+faizmuzaki@users.noreply.github.com>
2022-12-13 21:28:42 +0700

    Update add search pos

--- uap_kodepos.py
+++ uap_kodepos.py
@@ -8,7 +8,7 @@ class Provinsi:
         self.linkProv = linkProv
         self.getProv = getProv
         self.prov = prov
- 
+    
     def getKeyProv(self):
         for key, value in self.prov.items():
             if self.namaProv == value:
@@ -16,6 +16,7 @@ class Provinsi:
  
         return "Key not found"
 
+ 
 class Kota:
     def __init__(self, namaKota):
         self.namaKota = namaKota
@@ -29,6 +30,25 @@ class Kota:
                 return key
  
         return "Key not found"
+ 
+
+
+class Pos:
+    def __init__(self, namaLurah):
+        self.namaLurah = namaLurah
+        self.linkPos = linkPos
+        self.hasilPos = hasilPos
+        self.pos = pos
+ 
+    def getKeyPos(self):
+        for i in pos:
+            if self.namaLurah == i['kelurahan']:
+                print('\nKecamatan\t:', i['kecamatan'], '\nKelurahan\t:', i['kelurahan'], 
+                '\nKode Pos\t:', i['kodepos'], '\n')
+                f = open("history.txt", "a")
+                f.write("\nKecamatan\t:"+ i['kecamatan']+ "\nKelurahan\t:"+ i['kelurahan']+"\nKode Pos\t:"+ i['kodepos']+ "\n")
+                f.close()
+
 
 while(True):
     os.system('cls')
@@ -61,7 +81,7 @@ while(True):
         prov1 = Provinsi(namaProv1.title())
         prov1.getKeyProv()
         keyProv = prov1.getKeyProv()
-        
+
         # Kota/Kabupaten
         linkKota = "https://kodepos-2d475.firebaseio.com/list_kotakab/"+keyProv+".json"
         hasilKota = requests.get(linkKota)
@@ -79,3 +99,43 @@ while(True):
         kota1 = Kota(namaKota1.title())
         kota1.getKeyKota()
         keyKota = kota1.getKeyKota()
+
+        # Kecamatan/Kelurahan
+        
+        linkPos = "https://kodepos-2d475.firebaseio.com/kota_kab/"+keyKota+".json"
+        hasilPos = requests.get(linkPos)
+        pos = hasilPos.json()
+
+        try:            
+            for i in pos:
+                print('Kecamatan\t:', i['kecamatan'], '\nKelurahan\t:', i['kelurahan'], '\n')
+        except TypeError:
+            print("\nInvalid Input\n")
+            x = input()
+            continue
+
+        namaLurah1 = input("Masukkan Nama Kelurahan\t: ")
+        pos1 = Pos(namaLurah1.title())
+        pos1.getKeyPos()
+        x = input()
+        
+    elif pilih == 2:
+            f = open("credits.txt", "w")
+            f.write("""Created by
+            Faiz Muzakki
+            Muhamad Iqbal Widiansyah
+            Muhamad Ramadhan Kamal""")
+            f = open("credits.txt", "r")
+            print(f.read())
+            x = input()
+            f.close()
+
+    elif pilih == 3:
+        print("\nprogram telah berakhir\n")
+        quit()
+
+    else:
+        print("Tidak ada di pilihan")
+        x = input()
+
+    
